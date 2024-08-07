c89f0d9eadc1e0211dc6a3551ffe9f8a0a4c1bfd
RubinhoSilva <rubinho142@gmail.com>
2024-08-06 22:39:28 -0300

    Implementando boss

--- components/move_component.gd
+++ components/move_component.gd
@@ -4,5 +4,16 @@ extends Node
 @export var actor: Node2D
 @export var velocity: Vector2
 
+var margin = 14
+var left_border = 0
+var right_border = ProjectSettings.get_setting("display/window/size/viewport_width")
+
 func _process(delta: float) -> void:
-	actor.translate(velocity * delta)
+	# Get the actor's current position
+	var actor_position = actor.position
+
+	# Move the actor based on the current velocity
+	actor_position += velocity * delta
+
+	# Update the actor's position
+	actor.position = actor_position
