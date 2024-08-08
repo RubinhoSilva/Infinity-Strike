1731249cb41e6ad894805c504cc4c19e386cec9c
RubinhoSilva <rubinho142@gmail.com>
2024-08-08 19:39:41 -0300

    Implementando boss

--- bosses/boss_one.gd
+++ bosses/boss_one.gd
@@ -1,12 +1,18 @@
 class_name BossOne
 extends Boss
 
+@onready var fire_rate_timer: Timer = $FireRateTimer
+@onready var left_muzzle: Marker2D = $LeftMuzzle
+@onready var right_muzzle: Marker2D = $RightMuzzle
+@onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
+@onready var scale_component: = $ScaleComponent as ScaleComponent
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _ready() -> void:
+	super()
+	fire_rate_timer.timeout.connect(fire_lasers)
+	
+	
+func fire_lasers() -> void:
+	spawner_component.spawn(left_muzzle.global_position)
+	spawner_component.spawn(right_muzzle.global_position)
+	scale_component.tween_scale()
