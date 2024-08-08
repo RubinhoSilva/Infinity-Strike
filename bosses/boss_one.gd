b39a8bb7c6ccead2e1becf9e2a905fb3ea7bddc8
RubinhoSilva <rubinho142@gmail.com>
2024-08-08 19:42:21 -0300

    Implementando boss

--- bosses/boss_one.gd
+++ bosses/boss_one.gd
@@ -5,7 +5,7 @@ extends Boss
 @onready var left_muzzle: Marker2D = $LeftMuzzle
 @onready var right_muzzle: Marker2D = $RightMuzzle
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
-@onready var scale_component: = $ScaleComponent as ScaleComponent
+
 
 func _ready() -> void:
 	super()
@@ -15,4 +15,3 @@ func _ready() -> void:
 func fire_lasers() -> void:
 	spawner_component.spawn(left_muzzle.global_position)
 	spawner_component.spawn(right_muzzle.global_position)
-	scale_component.tween_scale()
