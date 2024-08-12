423be05310954972e8e649a73d9a0d661b87320d
RubinhoSilva <rubinho142@gmail.com>
2024-08-12 20:08:52 -0300

    Implementando bonus

--- bonus/bonus_random.gd
+++ bonus/bonus_random.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var DamageBonusScene: PackedScene
+@export var HealthBonusScene: PackedScene
 
 @export var game_stats: GameStats
 
@@ -13,7 +14,12 @@ var screen_width = ProjectSettings.get_setting("display/window/size/viewport_wid
 # Called when the node enters the scene tree for the first time.
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	spaw_bonus(DamageBonusScene, Vector2(randf_range(margin, screen_width-margin), -16))
+	var number_rand = randi() % 3 + 1
+	
+	if (number_rand == 1):
+		spaw_bonus(DamageBonusScene, Vector2(randf_range(margin, screen_width-margin), -16))
+	elif (number_rand == 2):
+		spaw_bonus(HealthBonusScene, Vector2(randf_range(margin, screen_width-margin), -16))
 
 
 func spaw_bonus(bonus_scene: PackedScene, position: Vector2) -> void:
