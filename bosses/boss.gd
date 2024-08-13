3fa0b1b2897a55a2bae7229919f73e7ba52ce2b0
RubinhoSilva <rubinho142@gmail.com>
2024-08-13 19:48:30 -0300

    Implementando boss

--- bosses/boss.gd
+++ bosses/boss.gd
@@ -1,13 +1,24 @@
 class_name Boss
 extends Node2D
 
+@onready var stats_component: = $StatsComponent as StatsComponent
+@onready var move_component: = $MoveComponent as MoveComponent
 @onready var scale_component: = $ScaleComponent as ScaleComponent
+@onready var flash_component: = $FlashComponent as FlashComponent
+@onready var shake_component: = $ShakeComponent as ShakeComponent
+@onready var hurtbox_component: = $HurtboxComponent as HurtboxComponent
+@onready var hitbox_component: = $HitboxComponent as HitboxComponent
+@onready var destroyed_component: = $DestroyedComponent as DestroyedComponent
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+
+func _ready() -> void:
+	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent):
+		scale_component.tween_scale()
+		flash_component.flash()
+		shake_component.tween_shake()
+	)
+	stats_component.no_health.connect(queue_free)
+
