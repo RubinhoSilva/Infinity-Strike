dc73c27f1dec235f2ba4a40e0b28fe744e8ac582
RubinhoSilva <rubinho142@gmail.com>
2024-09-04 00:27:20 -0300

    Implementando nível 3

---
+++ enemy_ships/squads/level_three/squad_eight.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var enemy_one: = $EnemyOne as Node
+@onready var enemy_two: = $EnemyTwo as Node
+@onready var enemy_three: = $EnemyThree as Node
+
+func _ready() -> void:
+	var sign_ = (randi() % 2) * 2 - 1
+	var velocity = randi_range(5, 25)
+	
+	if enemy_one:
+		enemy_one.get_node('MoveComponent').velocity.y = velocity
+		
+	if enemy_two:
+		enemy_two.get_node('MoveComponent').velocity.y = velocity
+		
+			
+	if enemy_three:
+		enemy_three.get_node('MoveComponent').velocity.y = velocity
