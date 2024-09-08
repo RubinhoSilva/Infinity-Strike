a92d819bf0bac5dca6864643aba5041f4d239058
Joao Mateus <joaomateus.mu@gmail.com>
2024-09-08 02:30:56 -0300

    Adicionando som de explosão, adicionando musicas das fases 2 a 4 e ajustando volumes da colisão

--- components/onetime_animated_effect.gd
+++ components/onetime_animated_effect.gd
@@ -1,11 +1,15 @@
 # Give the component a class name so it can be instanced as a custom node
 class_name OnetimeAnimatedEffect
 
+
+
 # This effect exports animated sprite so we can give it a sprite frames resource (to animate the effect)
 # and so we can connect to the animation_finished signal to free it
 extends AnimatedSprite2D
+@onready var explosion_fx = $explosion
 
 func _ready() -> void:
 	# Free this node when the animation is finished
+	explosion_fx.play()
 	animation_finished.connect(queue_free)
 	animation_looped.connect(queue_free)
--- world.gd
+++ world.gd
@@ -2,22 +2,31 @@ extends Node2D
 
 @export var game_stats: GameStats
 
+@onready var music_player: AudioStreamPlayer
 @onready var ship: Node2D = $Ship
 @onready var score_label: Label = $ScoreLabel
 @onready var health_label: Label = $HealthLabel
 @onready var damage_label: Label = $DamageLabel
 @onready var bg_fase_1 = $bg_fase1
+@onready var bg_fase_2 = $bg_fase2
+@onready var bg_fase_3 = $bg_fase3
+@onready var bg_fase_4 = $bg_fase4
+
 
 
 func _ready() -> void:
 	randomize()
-	bg_fase_1.play()
+
 	update_score_label(game_stats.score)
 	update_health_label(ship.get_node('StatsComponent').health)
 	update_damage_label(ship.get_node('StatsComponent').damage)
 	
 	game_stats.score_changed.connect(update_score_label)
 	ship.get_node('StatsComponent').health_changed.connect(update_health_label)
+	
+	game_stats.level_changed.connect(on_level_changed)
+	
+	on_level_changed(game_stats.level)
 
 	
 	ship.tree_exiting.connect(func():
@@ -33,3 +42,17 @@ func update_health_label(new_health: int) -> void:
 
 func update_damage_label(new_damage: int) -> void:
 	damage_label.text = "Damage: " + str(new_damage)
+	
+func on_level_changed(new_level: int) -> void:
+	if new_level == 1:
+		bg_fase_2.stop()  # Para a música da fase 2, se estiver tocando
+		bg_fase_1.play()  # Inicia a música da fase 1
+	elif new_level == 2:
+		bg_fase_1.stop()  # Para a música da fase 1
+		bg_fase_2.play()  # Inicia a música da fase 2
+	elif new_level == 3:
+		bg_fase_2.stop()
+		bg_fase_3.play()
+	elif new_level == 4:
+		bg_fase_3.stop()
+		bg_fase_4.play()
