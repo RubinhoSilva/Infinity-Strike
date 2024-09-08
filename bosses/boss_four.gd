c097894e6c13c76144b0fd8f7aef683e487fc3b9
RubinhoSilva <rubinho142@gmail.com>
2024-09-08 18:00:06 -0300

    Implementando boss 4

--- bosses/boss_four.gd
+++ bosses/boss_four.gd
@@ -2,11 +2,29 @@ class_name BossFour
 extends Boss
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var fire_rate_timer: Timer = $FireRateTimer
+@onready var muzzle1: Marker2D = $Muzzle1
+@onready var muzzle2: Marker2D = $Muzzle2
+@onready var muzzle3: Marker2D = $Muzzle3
+@onready var muzzle4: Marker2D = $Muzzle4
+@onready var muzzle5: Marker2D = $Muzzle5
+@onready var muzzle6: Marker2D = $Muzzle6
+@onready var muzzle7: Marker2D = $Muzzle7
+@onready var muzzle8: Marker2D = $Muzzle8
+@onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
+#@onready var laserboss_sfx = $laserboss_sfx
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _ready() -> void:
+	super()
+	fire_rate_timer.timeout.connect(fire_lasers)
+	
+	
+func fire_lasers() -> void:
+	spawner_component.spawn(muzzle1.global_position)
+	spawner_component.spawn(muzzle2.global_position)
+	spawner_component.spawn(muzzle3.global_position)
+	spawner_component.spawn(muzzle4.global_position)
+	spawner_component.spawn(muzzle5.global_position)
+	spawner_component.spawn(muzzle6.global_position)
+	#laserboss_sfx.play()
