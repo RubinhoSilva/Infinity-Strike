6773c213dbff2fc4ddd17cbda859116d4caca559
RubinhoSilva <rubinho142@gmail.com>
2024-09-15 16:16:04 -0300

    Finalizado

--- world.gd
+++ world.gd
@@ -7,6 +7,8 @@ extends Node2D
 @onready var score_label: Label = $ScoreLabel
 @onready var health_label: Label = $HealthLabel
 @onready var damage_label: Label = $DamageLabel
+@onready var new_level_label: Label = $NewLevelLabel
+@onready var show_new_level_label_timer: Timer = $ShowNewLevelLabelTimer
 @onready var bg_fase_1 = $bg_fase1
 @onready var bg_fase_2 = $bg_fase2
 @onready var bg_fase_3 = $bg_fase3
@@ -33,7 +35,10 @@ func _ready() -> void:
 		await get_tree().create_timer(1.0).timeout
 		get_tree().change_scene_to_file("res://menus/game_over.tscn")
 	)
-#
+
+func _process(delta):
+	new_level_label.visible = !show_new_level_label_timer.is_stopped()
+
 func update_score_label(new_score: int) -> void:
 	score_label.text = "Score: " + str(new_score)
 	
@@ -48,11 +53,18 @@ func on_level_changed(new_level: int) -> void:
 		bg_fase_2.stop()  # Para a música da fase 2, se estiver tocando
 		bg_fase_1.play()  # Inicia a música da fase 1
 	elif new_level == 2:
+		new_level_label.text = "Nível 1 Concluído"
 		bg_fase_1.stop()  # Para a música da fase 1
 		bg_fase_2.play()  # Inicia a música da fase 2
 	elif new_level == 3:
+		new_level_label.text = "Nível 2 Concluído"
 		bg_fase_2.stop()
 		bg_fase_3.play()
 	elif new_level == 4:
+		new_level_label.text = "Nível 3 Concluído"
 		bg_fase_3.stop()
 		bg_fase_4.play()
+		
+	if new_level != 1:
+		show_new_level_label_timer.wait_time = 5
+		show_new_level_label_timer.start()
